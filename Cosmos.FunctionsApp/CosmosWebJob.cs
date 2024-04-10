e684f41ab879670214c33e80a47531d80ccf2c41
Solly Fathi <sfathi_dt@qicdt.com>
2024-04-10 14:10:27 +1000

    Update Cosmos functions

--- Cosmos.FunctionsApp/CosmosWebJob.cs
+++ Cosmos.FunctionsApp/CosmosWebJob.cs
@@ -26,32 +26,25 @@ namespace Cosmos.FunctionsApp
                 databaseName: Constants.DatabaseName,
                 containerName: Constants.ContainerName,
                 Connection = Constants.CosmosConnectionString)]
-            List<Candidate> candidates)
+            IReadOnlyList<Candidate> input)
         {
             var request = await new StreamReader(req.Body).ReadToEndAsync();
             var candidate = JsonConvert.DeserializeObject<Candidate>(request)!;
 
-            if (candidates == null || candidates.Any(o => o.Id == candidate.Id)) return null!;
-
             _logger.LogInformation("Create:{FullName}, ID:{Id}", candidate.FullName, candidate.Id);
 
             // Cosmos Output
-            candidates.Add(new Candidate { Id = candidate.Id });
-
-            return candidates
-                .Where(o => o.Id == candidate.Id)
-                .Select(o => new
-                {
-                    id = o.Id,
-                    lastName = candidate.LastName,
-                    firstName = candidate.FirstName,
-                    email = candidate.Email,
-                    balance = candidate.Balance,
-                    points = candidate.Points,
-                    registrationDate = candidate.RegistrationDate,
-                    isActive = candidate.IsActive,
-                    technologies = candidate.Technologies
-                });
+            return input.Select(p => new { 
+                id = candidate.Id,
+                firstName = candidate.FirstName,
+                lastName = candidate.LastName,
+                email = candidate.Email,
+                balance = candidate.Balance,
+                points = candidate.Points,
+                registrationDate = candidate.RegistrationDate,
+                isActive = candidate.IsActive,
+                technologies = candidate.Technologies
+            });
         }
 
         [Function("updateCandidate")]
@@ -65,17 +58,17 @@ namespace Cosmos.FunctionsApp
                 databaseName: Constants.DatabaseName,
                 containerName: Constants.ContainerName,
                 Connection = Constants.CosmosConnectionString)]
-            IList<Candidate> candidates)
+            IReadOnlyList<Candidate> input)
         {
             var request = await new StreamReader(req.Body).ReadToEndAsync();
             var candidate = JsonConvert.DeserializeObject<Candidate>(request)!;
 
-            if (candidates == null || !candidates.Any(o => o.Id == id.ToString())) return null!;
+            if (input == null || !input.Any(o => o.Id == id.ToString())) return null!;
 
             _logger.LogInformation("Update:{FullName}, ID:{id}", candidate.FullName, id);
 
             // Cosmos Output
-            return candidates
+            return input
                 .Where(o => o.Id == id.ToString())
                 .Select(o => new
                 {
