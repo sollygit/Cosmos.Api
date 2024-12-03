9f1c55afd296ab2777532c4a3f88e6041ee0857e
Solly <post2solly@gmail.com>
2024-12-03 21:16:58 +1000

    env prod update

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://cosmosapp-api.azurewebsites.net',
+  baseUrl: 'https://cosmos-api.azurewebsites.net',
   functionUrl: 'https://cosmos-functions.azurewebsites.net'
 };
